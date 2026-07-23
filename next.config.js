9de5deb358af11c4d2dea1cd61a20dc8dd75f355
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-07-23 10:41:00 +0600

    fix: add main domain on nextjs config

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const SECURITY_HEADERS = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob: https://images.unsplash.com https://*.googleusercontent.com http://localhost:5000 https://i.ytimg.com https://img.youtube.com",
       "font-src 'self' https://fonts.gstatic.com data:",
-      "connect-src 'self' http://localhost:5000 https://*.sslcommerz.com https://translate.googleapis.com https://api.allorigins.win https://corsproxy.io https://api.codetabs.com https://youtu.be https://www.youtube.com https://*.googleapis.com https://api.darulquranfoundation.org https://img.youtube.com",
+      "connect-src 'self' http://localhost:5000 https://*.sslcommerz.com https://translate.googleapis.com https://api.allorigins.win https://corsproxy.io https://api.codetabs.com https://youtu.be https://www.youtube.com https://*.googleapis.com https://api.darulquranfoundation.org https://api.darulquranwelfareassociation.org https://img.youtube.com",
       "frame-src 'self' https://www.youtube.com https://youtube.com https://*.youtube.com https://img.youtube.com",
       "frame-ancestors 'none'",
       "object-src 'none'",
@@ -20,7 +20,10 @@ const SECURITY_HEADERS = [
   { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
   { key: "X-Frame-Options", value: "DENY" },
   { key: "X-Content-Type-Options", value: "nosniff" },
-  { key: "Permissions-Policy", value: "camera=(), geolocation=(), microphone=()" },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), geolocation=(), microphone=()",
+  },
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000; includeSubDomains; preload",
